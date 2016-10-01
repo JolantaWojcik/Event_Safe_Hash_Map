34ec7784a67d2ed9a6d4c957ece5dd3a4d414bb2
jola <jola.j.wk@gmail.com>
2016-10-01 15:26:52 +0200

    first commit

---
+++ src/Event.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Event {
+	//(String miasto, String adres, ArrayList<java.util.Date> dniWystepowania)
+	public static final class EventDesc{
+		private final String name;
+		private final String address;
+		private final ArrayList<Date> datesOfEvents;
+		
+		public EventDesc(String name, String address, ArrayList<Date> datesOfEvents) {
+			super();
+			this.name = name;
+			this.address = address;
+			this.datesOfEvents = new ArrayList<Date>(); //datesOfEvents;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public ArrayList<Date> getDaysOfEvents() {
+			return datesOfEvents;
+		}
+		
+		public EventDesc setName(String name){
+			return new EventDesc(name, address,datesOfEvents);
+		}
+		
+		public EventDesc setAddress(String address){
+			return new EventDesc(name, address,datesOfEvents);
+		}
+		
+		public EventDesc setDate(String name){
+			return new EventDesc(name, address,datesOfEvents);
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((address == null) ? 0 : address.hashCode());
+			result = prime * result + ((datesOfEvents == null) ? 0 : datesOfEvents.hashCode());
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			EventDesc other = (EventDesc) obj;
+			if (address == null) {
+				if (other.address != null)
+					return false;
+			} else if (!address.equals(other.address))
+				return false;
+			if (datesOfEvents == null) {
+				if (other.datesOfEvents != null)
+					return false;
+			} else if (!datesOfEvents.equals(other.datesOfEvents))
+				return false;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "EventDesc [name=" + name + ", address=" + address + ", datesOfEvents=" + datesOfEvents + "]";
+		}
+		
+		
+	}
+}
+	
---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main{
+	private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
+	/*
+	 * Stworz klase Wydarzenie (String miasto, String adres, ArrayList<java.util.Date> dniWystepowania)
+	 * - chcemy ¿eby ta klasa by³a ca³kowicie immutable
+	 * je¿eli chcesz zrobiæ settery to powinny one zwracac now¹ instacjê Wydarzenia (zmienion¹)
+	 */
+	  public static void main(String[] args) {
+		//to jest zle pewnie:
+		//Event.EventDesc
+		//jak poprawnie przeniesc final static do innej klasy?
+
+		HashMap<Event.EventDesc, String> eventsMap = new HashMap<>();
+		ArrayList<Date> datesOfEvent1 = new ArrayList<Date>();
+		ArrayList<Date> datesOfEvent2 = new ArrayList<Date>();
+		ArrayList<Date> datesOfEvent3 = new ArrayList<Date>();
+		
+		try {
+			
+			datesOfEvent1.add(DATE_FORMATTER.parse("2016-12-31"));
+			datesOfEvent1.add(DATE_FORMATTER.parse("2017-12-31"));
+			
+			datesOfEvent2.add(DATE_FORMATTER.parse("2015-12-24"));
+			datesOfEvent2.add(DATE_FORMATTER.parse("2014-12-24"));
+			datesOfEvent2.add(DATE_FORMATTER.parse("2013-12-24"));
+			
+			datesOfEvent3.add(DATE_FORMATTER.parse("1996-12-01"));
+			datesOfEvent3.add(DATE_FORMATTER.parse("1996-12-01"));
+			
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Event.EventDesc e1 = new Event.EventDesc("new year party", "krk", datesOfEvent1);
+		Event.EventDesc e2 = new Event.EventDesc("christmas party", "krk", datesOfEvent2);
+		Event.EventDesc e3 = new Event.EventDesc("birthday", "krk", datesOfEvent3);
+		
+		eventsMap.put(e1, "new year");
+		eventsMap.put(e2, "christmas");
+		eventsMap.put(e3, "birthday");
+		
+		System.out.println("test1: " + eventsMap.get(e1)); 
+		System.out.println("test2: " + eventsMap.get(e2));
+		System.out.println("test2: " + eventsMap.get(e3));
+
+		e1.setName("birthday");
+
+		System.out.println("###");
+		System.out.println(eventsMap.keySet());
+		System.out.println("New event one: " + e1);
+	}
+
+}
